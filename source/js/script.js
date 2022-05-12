9c6a74ffe9e3753f99b26dba84eaf52f30969133
Maria Kovalenko <mi.kovalenko@mail.ru>
2022-05-12 22:37:38 +0200

    пофиксила часть багов

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,20 @@ let navMain = document.querySelector('.header__nav');
 let navToggle = document.querySelector('.header__toggle');
 
 navMain.classList.remove('header__nav--nojs');
+navToggle.classList.remove('header__toggle--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('header__nav--closed')) {
     navMain.classList.remove('header__nav--closed');
     navMain.classList.add('header__nav--open');
+    navToggle.classList.add('header__toggle--active');
+    navToggle.classList.remove('header__toggle--inactive');
   } else {
     navMain.classList.add('header__nav--closed');
     navMain.classList.remove('header__nav--open');
+    navToggle.classList.remove('header__toggle--active');
+    navToggle.classList.add('header__toggle--inactive');
   }
-});
+}
+);
+
